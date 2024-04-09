ba53f50035db271381e9578c9c42a39d74aa90ff
YuichiKamishiro <YuichiKamishiro@gmail.com>
2024-04-09 11:14:04 +0300

    CSS Added

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,20 @@
-use sysinfo::{Components, System};
-use gtk::{gio::ApplicationFlags, prelude::*, Application, ApplicationWindow, Label, Notebook};
+use sysinfo::System;
+
+use gtk::{gdk, glib, CssProvider, prelude::*, ApplicationWindow, Label, Notebook};
+
+use adw::Application;
 
 fn get_info() ->  String{
     let mut sys = System::new_all();
     sys.refresh_all();
 
-    format!("Total mem {} mb", sys.total_memory() / (1024 * 1024)) 
+    format!("Total mem {} MB\nFree mem {} MB\nUsed mem {} MB",
+        sys.total_memory() / (1024 * 1024),
+        sys.free_memory() / (1024 * 1024),
+        sys.used_memory() / (1024 * 1024))
 }
 
-fn draw_gui(app: &Application) {
+fn draw_gui(app: &Application){
     let mem_label = Label::new(Some(get_info().as_str()));
 
     let tabs = Notebook::new();
@@ -26,11 +32,23 @@ fn draw_gui(app: &Application) {
 
 const ID: &str = "ID";
 
-fn main() {
+fn load_css() {
+    let provider = CssProvider::new();
+    provider.load_from_data(include_str!("style.css"));
+    gtk::style_context_add_provider_for_display(
+        &gdk::Display::default().expect("Could not connect to a display."),
+        &provider,
+        gtk::STYLE_PROVIDER_PRIORITY_APPLICATION,
+    );
+}
+
+fn main() -> glib::ExitCode{
     let app = Application::builder()
         .application_id(ID)
         .build();
-    app.connect_activate(draw_gui);
 
-    app.run();
+    app.connect_startup(|_| load_css());
+    app.connect_activate(draw_gui);
+    
+    app.run()
 }
