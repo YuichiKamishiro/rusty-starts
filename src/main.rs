cc55568d54d43b7e8c4445d9d7aa2e62c7ed549c
YuichiKamishiro <YuichiKamishiro@gmail.com>
2024-04-21 21:42:13 +0300

    Added DiskInfo into List

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
+use my_application::run;
+
 mod my_application;
 mod page_info;
 
-use my_application::my_application::run;
-
 use gtk::glib;
-fn main() -> glib::ExitCode{
+fn main() -> glib::ExitCode {
     run()
 }
